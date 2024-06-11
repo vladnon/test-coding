ea4d8fc5d700ac31bcea8a033bb964913daff1fb
Vlad1ckPON <kharkovervladimir@gmail.com>
2024-06-12 02:26:56 +0300

    started to study java

---
+++ java/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+   public static void main(String[] args) {
+      Scanner scan = new Scanner(System.in);
+      int num0 = scan.nextInt();
+      int num1 = 234234;
+      num1++;
+      if (num0 > num1){
+         System.out.println("I dont undestand anything");
+      }else{
+         System.out.println("I think i undestand this fucking syntax");
+      } 
+   }
+}
