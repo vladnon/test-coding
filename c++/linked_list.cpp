bd60ee16f0c9924e4a03362411839d14b5a379d0
Vlad1ckPON <kharkovervladimir@gmail.com>
2024-11-07 00:27:01 +0300

    method remove was fixed

--- c++/linked_list.cpp
+++ c++/linked_list.cpp
@@ -5,86 +5,87 @@
 using namespace std;
 
 struct Node {
-  int value;
-  Node *next = NULL;
+    int value;
+    Node *next = NULL;
 };
 
 class LinkedList {
-  Node *head;
+    Node *head;
 
-public:
-  LinkedList() { head = NULL; }
-  void append(int value) {
-    Node *new_node = new Node();
-    new_node->value = value;
+  public:
+    LinkedList() { head = NULL; }
+    void append(int value) {
+        Node *new_node = new Node();
+        new_node->value = value;
 
-    if (!this->head) {
-      new_node->next = head;
-      head = new_node;
+        if (!this->head) {
+            new_node->next = head;
+            head = new_node;
 
-    } else {
-      Node *cur_node = head;
-      while (cur_node->next) {
-        cur_node = cur_node->next;
-      }
-      cur_node->next = new_node;
-    }
-  }
-
-  string print() {
-    Node *node = head;
-    string result;
-    while (node) {
-      result += (to_string(node->value)) + " ";
-      node = node->next;
-    }
-    return result;
-  }
-
-  void removeFromBeginng() {
-    Node *node = head;
-    head = head->next;
-    delete node;
-  }
-
-  void remove(int value) {
-    if (!head) {
-      cout << "The list is empty" << endl;
-      return;
+        } else {
+            Node *cur_node = head;
+            while (cur_node->next) {
+                cur_node = cur_node->next;
+            }
+            cur_node->next = new_node;
+        }
     }
 
-    Node *node = head;
+    string print() {
+        Node *node = head;
+        string result;
+        while (node) {
+            result += (to_string(node->value)) + " ";
+            node = node->next;
+        }
+        return result;
+    }
 
-    if (node->value == value) {
-      removeFromBeginng();
+    void removeFromBeginng() {
+        Node *node = head;
+        head = head->next;
+        delete node;
     }
-    while (node->next) {
-      if (node->next->value == value) {
-        Node *node_to_delete = node->next;
-        node->next = node_to_delete->next;
-        delete node_to_delete;
-        break;
-      }
-      node = node->next;
+
+    void remove(int value) {
+        if (!head) {
+            cout << "The list is empty" << endl;
+            return;
+        }
+
+        Node *node = head;
+
+        if (node->value == value) {
+            removeFromBeginng();
+            return;
+        }
+        while (node->next) {
+            if (node->next->value == value) {
+                Node *node_to_delete = node->next;
+                node->next = node_to_delete->next;
+                delete node_to_delete;
+                break;
+            }
+            node = node->next;
+        }
     }
-  }
 };
 
 LinkedList createListFromArray(vector<int> arr, LinkedList list) {
-  for (int i = 0; i < arr.size(); i++) {
-    int num = arr[i];
-    list.append(num);
-  }
-  return list;
+    for (int i = 0; i < arr.size(); i++) {
+        int num = arr[i];
+        list.append(num);
+    }
+    return list;
 }
 
 int main() {
-  LinkedList list = LinkedList();
-  vector<int> arr(10000000000000000);
-  list = createListFromArray(arr, list);
-  // cout << "this is fine";
-  // list.remove(10);
+    LinkedList list = LinkedList();
+    vector<int> arr(10000000000000000);
+    list = createListFromArray(arr, list);
+    // cout << "this is fine";
+    // list.remove(10);
 
-  cout << "My linked list: " << list.print() << endl;
-  return 0;
+    cout << "My linked list: " << list.print() << endl;
+    return 0;
 }
